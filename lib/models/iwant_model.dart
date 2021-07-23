ebcc2e8ae9afd5fb89fa173c780c38707bdffd52
ThanjiraT <thanjira.tor@student.mahidol.ac.th>
2021-07-23 17:55:06 +0700

    มีเออเร่อมากมาย

---
+++ lib/models/iwant_model.dart
@@ -0,0 +1,44 @@
+class IwantModel {
+  //Verbs
+  late String verb1;
+  late String verb2;
+
+  //Foods
+  late String singledish_foods;
+  late String soup_foods;
+  late String fried_foods;
+  late String steamed_foods;
+  late String grilled_foods;
+  late String salads_foods;
+  late String desserts_foods;
+  late String fruits_foods;
+
+  //Drinks
+  late String hotdrinks_drinks;
+  late String colddrinks_drinks;
+  late String frappe_drinks;
+
+
+  //Method
+  IwantModel(
+      this.verb1, this.verb2, this.singledish_foods, this.soup_foods, this.fried_foods, this.steamed_foods, this.grilled_foods, this.salads_foods, this.desserts_foods, this.fruits_foods, this.hotdrinks_drinks, this.colddrinks_drinks, this.frappe_drinks);
+
+  IwantModel.fromMap(Map<String, dynamic> map) {
+    verb1 = map['verb1'];
+    verb2 = map['verb2'];
+
+    singledish_foods = map['Sdname'];
+    soup_foods = map['Spname'];
+    fried_foods = map['Fdname'];
+    steamed_foods = map['Stname'];
+    grilled_foods = map['Grname'];
+    salads_foods = map['Slname'];
+    desserts_foods = map['Dsname'];
+    fruits_foods = map['Funame'];
+
+    hotdrinks_drinks = map['Hdname'];
+    colddrinks_drinks = map['Cdname'];
+    frappe_drinks = map['Frname'];
+
+  }
+}
---
+++ lib/screens/iwant.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intern/models/iwant_model.dart';
+import 'package:intern/Favorite.dart';
+import 'package:intern/Help.dart';
+import 'package:intern/Home.dart';
+import 'package:intern/WantBuy.dart';
+import 'package:intern/WantDrink.dart';
+import 'package:intern/WantEat.dart';
+import 'package:intern/WantGo.dart';
+import 'package:intern/WantListen.dart';
+import 'package:intern/WantRead.dart';
+import 'package:intern/WantTalk.dart';
+import 'package:intern/WantWatch.dart';
+
+class Iwant extends StatefulWidget {
+  const Iwant({Key? key}) : super(key: key);
+
+  @override
+  _IwantState createState() => _IwantState();
+}
+
+class _IwantState extends State<Iwant> {
+  //Field
+  List<IwantModel> iwantModels = [];
+
+  //Method
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readIwverb1();
+  }
+
+  Future<void> readIwverb1() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference collectionReference = firestore
+        .collection('Iwant')
+        .doc('av9GjXDgd6HdvekrXdNQ')
+        .collection('Iwverb1');
+
+    await collectionReference.snapshots().listen((response) {
+      List<DocumentSnapshot> snapshots = response.docs;
+      for (var snapshot in snapshots) {
+        print('snapshot = $snapshot');
+        print('Iwant = ${snapshot['verb1']}');
+
+        IwantModel iwantModel = IwantModel.fromMap(snapshot.data());
+        setState(() {
+          iwantModels.add(iwantModel);
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          itemCount: iwantModels.length,
+          itemBuilder: (BuildContext buildContext, int index) {
+            return Text(iwantModels[index].verb1);
+          }),
+    );
+  }
+}
